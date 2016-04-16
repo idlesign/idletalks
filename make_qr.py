8733bf600276f56116839eafc26cc3a2141c6d41
idle sign <idlesign@yandex.ru>
2016-04-16 13:09:32 +0600

    Добавлен скрипт для упрощения создания QR кодов.

---
+++ make_qr.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+"""Creates qr.png in current directory, encoding text received from command line.
+
+Requires "qrcode" and Pillow packages installed:
+    > pip install qrcode Pillow
+
+Usage to encode "some text":
+    > ./make_qr.py some text
+
+"""
+from sys import argv
+import qrcode
+
+
+out_file = 'qr.png'
+text = ' '.join(argv[1:])
+
+print('Encoding "%s" as QR Code into "%s" ...' % (text, out_file))
+
+qr = qrcode.QRCode(version=3, box_size=12, border=2)
+
+qr.add_data(text)
+qr.make(fit=True)
+
+img = qr.make_image()
+img.save(out_file)
+
+print('Done')
