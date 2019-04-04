67d9f58c0a14bc55874a85a4499fe14d1bdf2cad
idle sign <idlesign@yandex.ru>
2019-04-04 18:16:03 +0700

    Обновлён reveal.js 3.8.0.

--- static/slides.js
+++ static/slides.js
@@ -1,25 +1,24 @@
 Reveal.initialize({
-    controls: true,
-    mouseWheel: true,
 
-    progress: true,
-    history: true,
+    defaultTiming: 120,
     center: true,
     transition: 'slide',
     slideNumber: 'c/t',
+    progress: true,
+    history: true,
+    hash: true,
 
-    keyboard: {
-        37: 'prev', // left arrow
-        39: 'next' // right arrow
-    },
+    width: '70%',
+    margin: 0,
+    minScale: 0.5,
+    maxScale: 1.3,
+
+    controls: true,
+    controlsTutorial: false,
+    mouseWheel: true,
+    hideInactiveCursor: true,
 
     dependencies: [
-        {
-            src: '../static/revealjs/lib/js/classList.js',
-            condition: function () {
-                return !document.body.classList;
-            }
-        },
         {
             src: '../static/revealjs/plugin/highlight/highlight.js',
             async: true,
